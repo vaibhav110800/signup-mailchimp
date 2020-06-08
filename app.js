4f608398ea5a79951448c4b2bccda7b934c131f0
Vaibhav Thakur <vibhu1108@gmail.com>
2020-06-08 12:11:44 +0530

    3rd ommit

--- app.js
+++ app.js
@@ -64,7 +64,7 @@ app.post("/failure",function(req,res){
 });
 
 app.listen(process.env.PORT || 3000, function(){
-	console.log("server running on port 3000");
+	console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
 
 //136bb694dc31f105c96bb12aef71313c-us4
